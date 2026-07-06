4495ec3bf95b4a456be427d168bb1bdf264e9645
Aran Farwq <127892644+AranFarwq118114@users.noreply.github.com>
2026-07-06 17:08:40 +0300

    First day

---
+++ Lesson 1/Function.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log("This is a simple JavaScript function example.");
+
+window.alert("Welcome to the JavaScript lesson!");
+
+document.getElementById("myH").innerHTML = "Welcome to JavaScript!";
+document.getElementById("myP").innerHTML = "JavaScript is fun!";
+document.getElementById("myH2").textContent = "Another Heading";
+document.getElementById("myP2").textContent = "Another Paragraph";
---
+++ Lesson 2/Script.js
@@ -0,0 +1,13 @@
+let firstname = `aran`;
+
+let lastname = `faruq`;
+
+let age = 23;
+
+let GPA = 3.5;
+
+let Major = `Computer Science`;
+
+
+// Katek varable agrenynawa la naw double quoteion pewist akat ${nawy variable} la naw string akat.
+console.log(`My name is ${firstname} ${lastname}. I am ${age} years old. My GPA is ${GPA} and I am majoring in ${Major}.`);
