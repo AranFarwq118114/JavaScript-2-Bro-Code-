d8ff3c7750e7ffe8c1adf09e86e2c86d0c7da607
Aran Farwq <127892644+AranFarwq118114@users.noreply.github.com>
2026-07-08 11:38:25 +0300

    3 lessson basar yakawa bas lesson 3 w 6 batalla chunka ayanzanm

---
+++ Lesson 5/Script.js
@@ -0,0 +1,36 @@
+let a = "5";
+let b = "5";
+let c = "5";
+
+
+a = Number(a);
+b  =String(b);
+c = Boolean(c);
+    
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+
+let e = 5;
+let f = 5;
+let g = 5;
+
+e = Number(e);
+f = String(f);
+g = Boolean(g);
+
+console.log(e, typeof e);
+console.log(f, typeof f);
+console.log(g, typeof g);
+
+let h = "";
+let i = "";
+let j = "";
+
+h = Number(h);
+i  =String(i);
+j = Boolean(j);
+
+console.log(h, typeof h);
+console.log(i, typeof i);
+console.log(j, typeof j);
