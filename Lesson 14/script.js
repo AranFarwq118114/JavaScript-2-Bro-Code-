afd56a91d4c6fd42e349c6f3c8b2437ec8678673
Aran Farwq <127892644+AranFarwq118114@users.noreply.github.com>
2026-07-11 09:07:20 +0300

    First for today

---
+++ Lesson 14/script.js
@@ -0,0 +1,47 @@
+
+
+let usename1 = "aran";
+
+// console.log(usename1.charAt(0));
+// console.log(usename1.indexOf("f"));
+// console.log(usename1.lastIndexOf("f"));
+// console.log(usename1.length);
+// console.log(usename1.trim());
+// console.log(usename1.toUpperCase());
+// console.log(usename1.toLowerCase());
+// console.log(usename1.repeat(3));
+
+
+// let result = usename1.startsWith(" ");
+
+// if(result){
+//     console.log("yes it starts with space");
+//   }else{
+//     console.log("no it does not start with space");
+// }
+    
+
+// let result = usename1.endsWith(" ");
+
+// if(result){
+//     console.log("yes it ends with space");
+//   }else{
+//     console.log("no it does not end with space");
+// }
+
+
+// let result = usename1.includes(" ");
+
+// if(result){
+//     console.log("yes it includes a space");
+//   }else{
+//     console.log("no it does not include a space");
+// }
+
+
+let phoneNumber = "123-456-7890";
+
+// phoneNumber = phoneNumber.replaceAll("-", " ");
+// `phoneNumber = phoneNumber.padStart(14, "0");
+// phoneNumber = phoneNumber.padEnd(14, "0");
+console.log(phoneNumber);
